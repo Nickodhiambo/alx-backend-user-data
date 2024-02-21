8a9251444ac436eade596653e66a2d1e29a5d9eb
Nick <nodhiambo01@gmail.com>
2024-02-21 10:59:42 +0000

    Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -41,3 +41,30 @@ class DB:
         self._session.commit()
         # self._session.close()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by keyword arguments
+        """
+        if not kwargs:
+            raise InvalidRequestError(f"Invalid")
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+        # self._session.close()
+        if user is None:
+            raise NoResultFound(f"Not found")
+
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update a user by keyword arguments
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if key not in ["id", "email", "hashed_password", "session_id",
+                           "reset_token"]:
+                raise ValueError(f"Invalid")
+
+            setattr(user, key, value)
+        self._session.commit()
+        # self._session.close()
+        return None
